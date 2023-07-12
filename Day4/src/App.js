8120139656608277372896d3d68bb892c05901de
NithyaSri04 <127287237+NithyaSri04@users.noreply.github.com>
2023-07-12 16:02:02 +0530

    Add files via upload

---
+++ Day4/src/App.js
@@ -0,0 +1,65 @@
+//import React from 'react';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import './App.css';
+//function App() {
+//  return (
+ //   <div className="App">
+ //     <h3>Build Sign Up & Login UI Template in React</h3>
+ //   </div>
+ // );
+//}
+//export default App;
+
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import Login from './components/login.component'
+import SignUp from './components/signup.component'
+import Homepage from './components/homepage.component'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                  <Link className="nav-link" to={'/home-page'}>
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/home-page" element={<Homepage />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default App
